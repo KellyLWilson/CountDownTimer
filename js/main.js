192791b58bd8a88ffea92fa123f6fce89105944e
KellyLWilson <60358839+KellyLWilson@users.noreply.github.com>
2020-02-20 20:57:56 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // The data/time we want to countdown to
-var countDownDate = new Date("Feburary 20, 2020 08:59:00").getTime();
+var countDownDate = new Date("Feburary 20, 2020 19:58:00").getTime();
 
 // Run myfunc every second
 var myfunc = setInterval(function() {
